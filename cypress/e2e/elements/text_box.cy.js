7487eabaee8bf4356f23a54ad717f4b4c400de35
Karan <kkr31398@gmail.com>
2023-06-17 23:18:12 +0530

    First Commit

---
+++ cypress/e2e/elements/text_box.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+describe("Input Test Box Automation", ()=>{
+    // it('navigates', () => {
+    //     cy.visit('https://demoqa.com')
+    //   })
+      
+    it('navigates to new origin', () => {
+    cy.visit('https://demoqa.com/elements')
+
+
+    cy.xpath("(//ul[@class='menu-list']//li)[1]").click()
+    cy.get('#userName-label')
+        .contains('Full Name')
+        .should('exist');
+    })
+})
